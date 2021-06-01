e7715720c3fca72eb4f1d09e1a30b16660087304
itcast <itcast@it>
2021-06-01 14:13:44 +0800

    volatile实验

--- src/JMM/VolatileDemo.java
+++ src/JMM/VolatileDemo.java
@@ -4,10 +4,33 @@ class Data{
     public void addTo(){
         number+=60;
     }
+    public synchronized void addplusplus(){
+        number++;
+    }
         }
-
+/*
+* 验证volatile的可见性seevolatileDemo
+* 验证volatile不保证原子性
+*      ：即该事件是不可分割的完整性的，中间不可以被加塞，保证动作的完整性
+* */
 public class VolatileDemo {
     public static void main(String[] args) {
+//        seevolatileDemo();
+    Data data = new Data();
+    for(int i = 0;i<100;i++){
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName()+"come in .....");
+            for (int index=0;index<2000;index++)
+            data.addplusplus();
+        },String.valueOf(i)).start();
+    }
+    while (Thread.activeCount()>2){
+        Thread.yield();
+    }
+        System.out.println(Thread.currentThread().getName()+"最终的结果为"+data.number);
+    }
+
+    private static void seevolatileDemo() {
         Data data = new Data();
         new Thread(()->{
             System.out.println(Thread.currentThread().getName()+"come in.......");
@@ -21,7 +44,6 @@ public class VolatileDemo {
         },"AAAAA").start();
         while(data.number==0){}
         System.out.println(data.number);
-
     }
 
 }
