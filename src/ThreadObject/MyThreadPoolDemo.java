bc12d7ea54f3aec996d61c1b5c9c7f1dca02e853
itcast <itcast@it>
2021-06-06 10:41:35 +0800

    Thread

--- src/ThreadObject/MyThreadPoolDemo.java
+++ src/ThreadObject/MyThreadPoolDemo.java
@@ -1,31 +1,50 @@
 package ThreadObject;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 //线程池的底层就是ThreadPoolExecutor
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
-//        System.out.println(Runtime.getRuntime().availableProcessors());
-//        ExecutorService executorService = Executors.newFixedThreadPool(5);//一池五个线程
-//        ExecutorService executorService = Executors.newSingleThreadExecutor();//一池一个线程
-        ExecutorService executorService = Executors.newCachedThreadPool();//一个池子处理n个线程
-//       模拟十个用户来办理业务，每一个请求就是一个来自外部的请求线程
+////        System.out.println(Runtime.getRuntime().availableProcessors());
+////        ExecutorService executorService = Executors.newFixedThreadPool(5);//一池五个线程
+////        ExecutorService executorService = Executors.newSingleThreadExecutor();//一池一个线程
+//        ExecutorService executorService = Executors.newCachedThreadPool();//一个池子处理n个线程
+////       模拟十个用户来办理业务，每一个请求就是一个来自外部的请求线程
+//        try {
+//            for (int i = 1;i<=10;i++){
+//                executorService.execute(()->{
+//                    System.out.println(Thread.currentThread().getName()+"\t办理业务");
+//                });
+////                try {
+////                    TimeUnit.MILLISECONDS.sleep(500);
+////                } catch (InterruptedException e) {
+////                    e.printStackTrace();
+////                }
+//            }
+//
+//        }finally {
+//                executorService.shutdown();
+//        }
+        ExecutorService executorService1 = new ThreadPoolExecutor(2,
+                5,100L,
+                TimeUnit.SECONDS,new LinkedBlockingDeque<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
         try {
-            for (int i = 1;i<=10;i++){
-                executorService.execute(()->{
-                    System.out.println(Thread.currentThread().getName()+"\t办理业务");
-                });
-//                try {
-//                    TimeUnit.MILLISECONDS.sleep(500);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-            }
+            for (int i = 0; i < 8; i++) {
+                  final  int tmp = i;
+                  executorService1.execute(()->{
+                      System.out.println(Thread.currentThread().getName()+"\t"+tmp);
 
+                          try {
+                              TimeUnit.SECONDS.sleep(4);
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+                  });
+            }
         }finally {
-                executorService.shutdown();
+            executorService1.shutdown();
         }
     }
 }
